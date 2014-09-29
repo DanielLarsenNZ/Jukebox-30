237d81b47477ea29ddc7e6e12b4a940267052964
Daniel Larsen <daniel@larsen.net.nz>
2014-09-29 21:39:50 +1300

    Basic UI for Jukebox create and list.

---
+++ Jukebox.Web/public/scripts/controllers/indexController.js
@@ -0,0 +1,25 @@
+﻿function indexController($scope, $http) {
+    $scope.loading = true;
+    
+    $http.get('/api/jukeboxes').success(function (data) {
+        $scope.jukeboxes = data;
+        $scope.loading = false;
+    }).error(function (data) {
+        $scope.error = data;
+        $scope.loading = false;
+    });
+
+    $scope.createJukebox = function () {
+        $scope.error = null;
+        $scope.loading = true;
+        
+        $http.post('/api/jukeboxes', {name: $scope.name}).success(function (data) {
+            $scope.jukeboxes.push(data);
+            $scope.loading = false;
+        }).error(function (data) {
+            $scope.error = data;
+            $scope.loading = false;
+        });
+    };
+
+}
