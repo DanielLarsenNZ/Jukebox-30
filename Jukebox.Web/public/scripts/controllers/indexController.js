c2d865efca70cfa24ceb4121a8326989dd23e522
Daniel Larsen <daniel@larsen.net.nz>
2014-09-30 08:08:44 +1300

    UI cleanup - index.html

--- Jukebox.Web/public/scripts/controllers/indexController.js
+++ Jukebox.Web/public/scripts/controllers/indexController.js
@@ -13,9 +13,13 @@
         $scope.error = null;
         $scope.loading = true;
         
-        $http.post('/api/jukeboxes', {name: $scope.name}).success(function (data) {
-            $scope.jukeboxes.push(data);
+        $http.post('/api/jukeboxes', { name: $scope.name, spotifyUsername: $scope.spotifyUsername }).success(function (data) {
+            
             $scope.loading = false;
+            //$scope.jukeboxes.push(data);
+
+            document.location = '/jukebox.html?id=' + data.RowKey;
+
         }).error(function (data) {
             $scope.error = data;
             $scope.loading = false;
