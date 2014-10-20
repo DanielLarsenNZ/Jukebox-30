94a2c3d74978352787e88a4013304433f5c6f455
Daniel Larsen <daniel@larsen.net.nz>
2014-10-20 22:34:49 +1300

    A better Jukebox list
    Oh, and I introduced multi-view routing!

--- Jukebox.Web/public/scripts/controllers/indexController.js
+++ Jukebox.Web/public/scripts/controllers/indexController.js
@@ -1,28 +1,32 @@
-﻿function indexController($scope, $http) {
-    $scope.loading = true;
-    
-    $http.get('/api/jukeboxes').success(function (data) {
-        $scope.jukeboxes = data;
-        $scope.loading = false;
-    }).error(function (data) {
-        $scope.error = data;
-        $scope.loading = false;
-    });
-
-    $scope.createJukebox = function () {
-        $scope.error = null;
+﻿app.controller('indexController', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
         $scope.loading = true;
-        
-        $http.post('/api/jukeboxes', { name: $scope.name, spotifyUsername: $scope.spotifyUsername }).success(function (data) {
-            
-            $scope.loading = false;
-            $scope.jukeboxes.push(data);
-            document.location = '/jukebox.html#?id=' + data.RowKey;
 
-        }).error(function (data) {
+        $http.get('/api/jukeboxes').success(function(data) {
+            $scope.jukeboxes = data;
+            $scope.loading = false;
+        }).error(function(data) {
             $scope.error = data;
             $scope.loading = false;
         });
-    };
 
-}
+        $scope.createJukebox = function() {
+            $scope.error = null;
+            $scope.loading = true;
+
+            $http.post('/api/jukeboxes', { name: $scope.name, spotifyUsername: $scope.spotifyUsername }).success(function(data) {
+
+                $scope.loading = false;
+                $scope.jukeboxes.push(data);
+                $location.path('/jukebox/' + data.RowKey);
+
+            }).error(function(data) {
+                $scope.error = data;
+                $scope.loading = false;
+            });
+        };
+
+    }
+]);
+
+
--- Jukebox.Web/routes/api.js
+++ Jukebox.Web/routes/api.js
@@ -1,85 +1,90 @@
-﻿var _jukeboxService = require('../services/jukeboxservice.js');
+﻿(function() {
+    "use strict";
+
+    var _jukeboxService = require('../services/jukeboxservice.js');
 
     // POST /api/jukeboxes
     // creates a Jukebox and returns the id
-exports.createJukebox = function(req, res) {
-	_jukeboxService.createJukebox(req.param('name'), req.param('spotifyUsername'), function(error, jukebox){
-		if (error) {
-			console.error(error.stack);
-			res.send(500, error.message);
-			return;
-		}
+    exports.createJukebox = function(req, res) {
+	    _jukeboxService.createJukebox(req.param('name'), req.param('spotifyUsername'), function(error, jukebox){
+		    if (error) {
+			    console.error(error.stack);
+			    res.send(500, error.message);
+			    return;
+		    }
 
-		res.json(jukebox);		
-	});	
-};
-
-// GET /api/jukeboxes
-// lists jukeboxes
-exports.listJukeboxes = function(req, res) {
-	_jukeboxService.listJukeboxes(20, function(error, jukeboxes){
-		if (error) {
-			console.error(error.stack);
-			res.send(500, error.message);
-			return;
-		}
-
-		res.json(jukeboxes);
-	});
-};
-
-// GET /api/jukeboxes/:id
-// retrieves a jukeboxe
-exports.getJukebox = function(req, res) {
-	_jukeboxService.getJukebox(req.param("id"), function(error, jukebox){
-		if (error) {
-			console.error(error.stack);
-			res.send(500, error.message);
-			return;
-		}
-
-		res.json(jukebox);
-	});
-};
-
-// GET /api/playlists?username=daniellarsennz
-    // Gets Spotify Playlists for a given username
-exports.getPlaylists = function (req, res) {
-    var spotify = require('../services/spotify.js');
-    
-    spotify.getPlaylists(req.query.username, function (error, response) {
-        if (error) {
-            console.error(error.stack);
-            res.send(500, error.message);
-            return;
-        }
-        
-        res.json(response);
-    });
-};
-
-    // POST /api/jukeboxes/:id/tracks {"username":"xxxx", "playlistId":"xxxx"}
-    // Adds the tracks from playlist xxxx to the Jukebox.
-exports.importPlaylist = function (req, res) {
-    // queue for import and return
-    var queue = require('../services/queuestorage.js');
-    
-    var message = {
-        jukeboxId: req.params.id,
-        username: req.param('username'),
-        playlistId: req.param('playlistId'),
-        whenCreated: new Date()
+		    res.json(jukebox);		
+	    });	
     };
 
-    queue.createMessage('import-playlist', JSON.stringify(message), function(error) {
-        if (error) {
-            console.error(error.stack);
-            res.send(500, error.message);
-            return;
-        }
+    // GET /api/jukeboxes
+    // lists jukeboxes
+    exports.listJukeboxes = function(req, res) {
+	    _jukeboxService.listJukeboxes(20, function(error, jukeboxes){
+		    if (error) {
+			    console.error(error.stack);
+			    res.send(500, error.message);
+			    return;
+		    }
 
-        console.log("process ID", process.pid);
+		    res.json(jukeboxes);
+	    });
+    };
 
-        res.send(200, "ok");
-    });
-};
+    // GET /api/jukeboxes/:id
+    // retrieves a jukeboxe
+    exports.getJukebox = function(req, res) {
+	    _jukeboxService.getJukebox(req.param("id"), function(error, jukebox){
+		    if (error) {
+			    console.error(error.stack);
+			    res.send(500, error.message);
+			    return;
+		    }
+
+		    res.json(jukebox);
+	    });
+    };
+
+    // GET /api/playlists?username=daniellarsennz
+        // Gets Spotify Playlists for a given username
+    exports.getPlaylists = function (req, res) {
+        var spotify = require('../services/spotify.js');
+    
+        spotify.getPlaylists(req.query.username, function (error, response) {
+            if (error) {
+                console.error(error.stack);
+                res.send(500, error.message);
+                return;
+            }
+        
+            res.json(response);
+        });
+    };
+
+        // POST /api/jukeboxes/:id/tracks {"username":"xxxx", "playlistId":"xxxx"}
+        // Adds the tracks from playlist xxxx to the Jukebox.
+    exports.importPlaylist = function (req, res) {
+        // queue for import and return
+        var queue = require('../services/queuestorage.js');
+    
+        var message = {
+            jukeboxId: req.params.id,
+            username: req.param('username'),
+            playlistId: req.param('playlistId'),
+            whenCreated: new Date()
+        };
+
+        queue.createMessage('import-playlist', JSON.stringify(message), function(error) {
+            if (error) {
+                console.error(error.stack);
+                res.send(500, error.message);
+                return;
+            }
+
+            console.log("process ID", process.pid);
+
+            res.send(200, "ok");
+        });
+    };
+})();
+
