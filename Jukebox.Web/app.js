9810928ba708cf40b8159b02ff9d9a5979434f19
Daniel Larsen <daniel@larsen.net.nz>
2014-10-28 14:45:45 +1300

    Offline mode v2

--- Jukebox.Web/app.js
+++ Jukebox.Web/app.js
@@ -9,7 +9,7 @@ dotenv.load();
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+//var user = require('./routes/user');
 var api = require('./routes/api');
 var playerSocket = require('./services/playerSocket');
 var http = require('http');
@@ -20,21 +20,22 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(express.methodOverride());
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// offline page middleware
-if (process.env.OfflineMode === "true") {
-    app.use(function (req, res, next) {
-        res.send(503, "Service Unavailable. Offline for maintenance - retry soon.");
-        console.log('OfflineMode = true. %s %s returned 503 \"Service Unavailable. Offline for maintenance - retry soon.\"', req.method, req.url);
-        //next();
+// middleware to return 503 for any api calls in offline mode.
+var isOffline = process.env.OfflineMode === "true";
+
+if (isOffline) {
+    app.use("/api", function (req, res) {
+        res.send(503, "Service unavailable. Offline for maintenance, retry soon.");
+        console.log('OfflineMode = true. %s %s returned 503 \"Service unavailable. Offline for maintenance, retry soon.\"', req.method, req.url);
     });
 }
 
@@ -45,9 +46,19 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/users', user.list);
+// routes
+// index route returns _index.html or offline.html if in offline mode.
+app.get("/", function (req, res) {
+    if (isOffline) {
+        res.status(503);
+        res.sendfile(path.join(__dirname, 'public/offline.html'));
+        console.log('OfflineMode = true. %s %s returned 503 /offline.html', req.method, req.url);
+        return;
+    }
 
-// dan's routes
+    res.sendfile(path.join(__dirname, 'public/_index.html'));
+});
+
 app.post('/api/jukeboxes', api.createJukebox);
 app.get('/api/jukeboxes', api.listJukeboxes);
 app.get('/api/jukeboxes/:id', api.getJukebox);
