3874494010d1a7381d95aa34b8247b77c4558315
Daniel Larsen <daniel@larsen.net.nz>
2015-03-29 14:06:59 +1300

    Fake Kafka MQ

---
+++ Jukebox.Web/services/fake-kafka.js
@@ -0,0 +1,62 @@
+﻿(function() {
+    "use strict";
+
+    // //////////
+    // Fake Kafka
+    // //////////
+    
+    //TODO: 2 files, 1 for Producer, 1 for Consumer. nullkafka exports those. See: https://github.com/SOHU-Co/kafka-node/blob/master/kafka.js
+
+    var _ = require("underscore");
+
+    var consumers = [];
+    var queueTime = 100;
+    
+    var FakeKafka = new function () { 
+
+        this.Producer = function () {
+            this.send = function (payloads, cb) {
+                _.each(payloads, function (payload) {
+                    var topicConsumers = _.filter(consumers, function (consumer) { return payload.topic === consumer.topic; });
+                
+                    if (topicConsumers.length == 0) {
+                        console.log("No consumers for {topic}. Message discarded.", payload.topic);
+                        cb("ok");
+                        return;
+                    }
+                
+                    var index = 0;  
+                    if (topicConsumers.length > 1) {
+                        index = (Math.floor(Math.random() * (topicConsumers.length - 1)) + 1) - 1;
+                    }
+                
+                    _.each(payload.messages, function (message) {
+                        setTimeout(function () { topicConsumers[index].onmessage(message); }, queueTime);
+                        console.log("Message for {topic} will be sent to Consumer {index}.", payload.topic, index);
+                    });
+
+                    cb("ok");
+                });
+            };
+        };
+    
+        this.Consumer = function (payloads) {
+            this.payloads = payloads;
+
+            this.on = function (name, cb) {
+                if (name = 'message') {
+                    _.each(this.payloads, function (payload) {
+                        consumers.push({ topic: payload.topic, onmessage: cb });
+                        console.log("Consumer registered for {topic}.", payload.topic);
+                    });
+                
+                    return;
+                }
+
+                throw "on " + name + " is not supported.";  
+            };
+        };
+    }
+
+    module.exports = FakeKafka;
+})();
--- Jukebox.Web/services/tracksservice.js
+++ Jukebox.Web/services/tracksservice.js
@@ -1,14 +1,17 @@
 ﻿(function() {
     "use strict";
 
-    var mq = require('./messagequeue.js');
+    var mq = require('./fake-kafka.js');
+    
+    // subscribe to tracks topic
     var Consumer = mq.Consumer;
-    var consumer = new Consumer("tracks");
+    var consumer = new Consumer([{ topic: "tracks" }]);
     consumer.on('message', function (message) {
         console.log(message);
     });
 
     module.exports.get = function (playlistId, cb) {
+        // publish to tracks topic
         var Producer = mq.Producer;
         var publisher = new Producer();
         publisher.send([{ topic: "tracks", messages: ["hello mq"] }], function (data) { cb(data); }); 
